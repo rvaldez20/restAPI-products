22011453166745327760c9fe080495629c441fa8
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-04 22:36:34 -0600

    Se crea el archvio index.routers para las rutas y se agrega en el index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const routes = require('./routes/index.routes')
 
 // crea el servidor
 const app = express();
+
+// rutas de la app
+app.use('/', routes());
+
+
 
 // puerto
 app.listen(5000);
