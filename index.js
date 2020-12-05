c6480cc2adfbda6a86419183bde4f7b98bbdeef1
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-04 23:06:05 -0600

    Se configura la conecxion a mongodb a la db restapis

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express');
-const routes = require('./routes/index.routes')
+const mongoose = require('mongoose');
+const routes = require('./routes/index.routes');
+
+//conectar con mongo
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/restapis', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
 
 // crea el servidor
 const app = express();
