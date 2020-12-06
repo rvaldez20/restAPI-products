4f69ea4ac68d245e126d5f491ca207a19c4dc8c2
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-05 22:50:44 -0600

    Se crea el controller y route para mostrar los clientes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 const clienteController =  require('../controllers/clienteController');
 
 module.exports = function() {
-   
    // Agrega nuevos clientes vos POST
    router.post('/clientes', clienteController.nuevoCliente);
-   
 
+   // Obtener todos los clientes
+   router.get('/clientes', clienteController.mostrarClientes);
+   
    return router;
 }
+
