560f77f60215cba3d777a00fdfeb380676c2ec6b
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-05 18:48:31 -0600

    Se crea el modelo Clientes

---
+++ models/Clientes.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const clientesSchema = new Schema({
+   nombre: {
+      type: String,
+      trim: true,
+   },
+   apellido: {
+      type: String,
+      trim: true
+   },
+   empresa: {
+      type: String,
+      trim: true
+   },
+   email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      trim: true
+   },
+   telefono: {
+      type: String,
+      trim: true
+   }
+},{
+   timestamps: true
+});
+
+module.exports = mongoose.model('Clientes', clientesSchema);
