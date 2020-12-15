887a1a160cce6e4f7abf4cde557cf47801207134
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2020-12-15 14:54:55 -0600

    Se implementa multer para subir archivos al servidor y se crea la funcionalidad para nuevo producto

--- controllers/productosController.js
+++ controllers/productosController.js
@@ -1,4 +1,44 @@
+const multer = require('multer');
+const shortid = require('shortid');
 const Productos = require('../models/Productos');
+
+const configuracionMulter = {
+   // limits: { fileSize: 100000 },
+   storage: fileStorage = multer.diskStorage({
+      destination: (req, file, cb) => {
+         cb(null, __dirname+'../../uploads/');
+      },
+      filename: (req, file, cb) => {
+         // console.log(file);
+         
+         // obtenemos la extensión del archvio
+         const extension = file.mimetype.split('/');
+         cb(null, `${shortid.generate()}.${extension[1]}`);
+      }
+   }),
+   fileFilter: (req, file, cb) => {
+      if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+         // el callback se ejecuta como true o false:
+         // true: cuando la imagen se acepta - false: 
+         cb(null, true);
+      } else {
+         cb(new Error('Formato no valido'), false);
+      }
+   },
+}
+
+// pasar la configuracion y el campo
+const upload = multer(configuracionMulter).single('imagen');
+
+// sube un archvio 
+exports.subirArchivo = (req, res, next) => {
+   upload(req, res, function(error) {
+      if (error){
+         res.json({mensaje: error})
+      }
+      return next();
+   })
+}
 
 // Agrega un nuevo producto
 exports.nuevoProducto = async (req, res, next) => {
@@ -8,6 +48,12 @@ exports.nuevoProducto = async (req, res, next) => {
    const producto = new Productos(req.body);
 
    try {
+
+      // si hay un archivo
+      if(req.file.filename) {
+         producto.imagen = req.file.filename
+      }
+
       await producto.save();
       res.json({mensaje: 'Se agrego un nuevo producto'});
    } catch (error) {
