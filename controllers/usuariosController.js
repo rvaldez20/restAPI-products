ba56fc004a431f1f4cc7f2683c1f96759d791375
rvaldez20 <30907228+rvaldez20@users.noreply.github.com>
2021-02-11 12:33:54 -0600

    Se trabajan en elcontrolador autenticarUsuario, se valida que este registrado el correo, que el password sea correcto, si es asi se genera el token con jwt

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -21,7 +21,45 @@ exports.registrarUsuario = async (req, res) => {
 }
 
 
-exports.autenticarUsuario = () => {
+exports.autenticarUsuario = async (req, res, next) => {
 
+   // buscar el usuario si existe
+   const {email, password} = req.body;   
+   const usuario = await Usuarios.findOne({ email });
+   
+   if(!usuario) {
+      // si el usuario NO existe
+      await res.status(401).json({mensaje: 'Ese usuario no esta registrado'});
+      next();
+   } else {
+      // si el usuario SI existe
+      // Primero verificamos si el password es correcto
+      // password -> viene del formulario | usuario.password -> es el passwor en la db
+      if(!bcrypt.compareSync(password, usuario.password)) {
+         //Si el password es incorrecto
+         await res.status(401).json({ mensaje: 'Password Incorrecto' });
+         next();
+      } else {
+         // Si password correcto se firma el token
+         /*
+            Para generar el token se usa jwt.sign({})
+            1.-Payload           -> son los datos del usuario que esta en la DB
+            2.- SecretPrivateKey -> es un string propio 
+            3.- Opctios          -> se configura fecha de expiracion
+         */
+         const token = jwt.sign({
+            email: usuario.email,
+            nombre: usuario.nombre,
+            id: usuario._id
+         }
+         , 'LLAVESECRETA',
+         {
+            expiresIn: '1h'
+         });
+
+         // retornamos el token
+         res.json({ token });
+      }
+   }
 
 }
